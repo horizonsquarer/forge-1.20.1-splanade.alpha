224104dbf876832ca5c6a9ce7dbd92c3aa9809ba
Horizonsquarer <cyberplays72@gmail.com>
2025-04-07 22:16:04 +0300

    Minor Commit

--- src/main/java/net/horizonsquarer/splanade/item/custom/CrystallisedTenebriteSolutionItem.java
+++ src/main/java/net/horizonsquarer/splanade/item/custom/CrystallisedTenebriteSolutionItem.java
@@ -1,29 +1,50 @@
 package net.horizonsquarer.splanade.item.custom;
 
+import net.horizonsquarer.splanade.item.ModItems;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
-public class CrystallisedTenebriteSolutionItem extends PotionItem {
+public class CrystallisedTenebriteSolutionItem extends Item {
     public CrystallisedTenebriteSolutionItem(Properties properties) {
         super(properties);
     }
+
     @Override
-    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity entity) {
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         if (!level.isClientSide) {
-            entity.addEffect(new MobEffectInstance(MobEffects.POISON, 100, 4)); // Poison V for 5 seconds
+            // Remove one from the held stack
+            ItemStack heldStack = player.getItemInHand(hand);
+            heldStack.shrink(1);
+
+            // Create the return items
+            ItemStack glassBottle = new ItemStack(Items.GLASS_BOTTLE);
+            ItemStack tenebriteDust = new ItemStack(ModItems.CRYSTALLISED_TENEBRITE_DUST.get());
+
+            // Try to add the glass bottle
+            if (!player.getInventory().add(glassBottle)) {
+                player.drop(glassBottle, false);
+            }
+
+            // Try to add the dust
+            if (!player.getInventory().add(tenebriteDust)) {
+                player.drop(tenebriteDust, false);
+            }
         }
-        return super.finishUsingItem(stack, level, entity);
+
+        return InteractionResultHolder.sidedSuccess(player.getItemInHand(hand), level.isClientSide());
     }
+
 
     @Override
     public Component getName(ItemStack stack) {
@@ -34,9 +55,10 @@ public class CrystallisedTenebriteSolutionItem extends PotionItem {
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
         tooltip.add(
                 Component.translatable("item.splanade.crystallised_tenebrite_solution_potion.tooltip")
-                        .withStyle(ChatFormatting.RED)
+                        .withStyle(ChatFormatting.GRAY)
         );
     }
 }
+
 
 
